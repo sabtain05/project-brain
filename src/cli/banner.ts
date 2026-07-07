c73dc77cbde619a569544f0dbacef303af9ca8d9
Sabtain Ali <sabtainalipk144@gmail.com>
2026-07-07 16:30:04 +0500

    Release v0.0.4

--- src/cli/banner.ts
+++ src/cli/banner.ts
@@ -8,7 +8,7 @@ export function showBanner(): void {
   console.log(chalk.cyan.bold("QUICKLYZER"));
   console.log(chalk.gray("===================================="));
 
-  console.log(chalk.green("v0.0.3"));
+  console.log(chalk.green("v0.0.4"));
   console.log(chalk.white("A CLI tool that understands and analyzes software projects in seconds."));
 
   console.log();
--- src/commands/about.ts
+++ src/commands/about.ts
@@ -10,7 +10,7 @@ export function aboutCommand() {
 A CLI tool that understands and analyzes software projects in seconds.
 
 Author : Sabtain Ali
-Version: 0.0.3
+Version: 0.0.4
 
 GitHub:
 https://github.com/sabtain05/quicklyzer
