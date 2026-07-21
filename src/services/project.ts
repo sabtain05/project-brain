1ed548d76e669b4888b12402a2d2886f9eddbfcf
Sabtain Ali <sabtainalipk144@gmail.com>
2026-07-21 11:55:44 +0500

    Import analyzeGit function for enhanced project analysis

--- src/services/project.ts
+++ src/services/project.ts
@@ -14,6 +14,7 @@ import {
 import { analyzeCode } from "./code.js";
 import { analyzeDependencies } from "./dependencies.js";
 import { analyzeDocumentation } from "./documentation.js";
+import { analyzeGit } from "./git.js";
 
 
 export interface ProjectInfo {
